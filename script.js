5829f6213833a0a569d192b3e1e26a85ab72d96f
li-kuan-gi <82380617+li-kuan-gi@users.noreply.github.com>
2022-06-27 04:15:58 +0000

    make class draggable by touch/click

--- script.js
+++ script.js
@@ -3,11 +3,7 @@ const draggables = document.getElementsByClassName('class');
 Array.from(draggables).forEach(
     element => {
         centralize(element);
-
-        let originTouchX;
-        let originTouchY;
-        element.addEventListener('touchstart', handleTouchStart);
-        element.addEventListener('touchmove', handleTouchMove);
+        makeDraggable(element);
     }
 );
 
@@ -16,6 +12,18 @@ function centralize(element) {
     rect = element.getBoundingClientRect();
     element.style.left = (parentRect.width - rect.width) / 2 + parentRect.x + 'px';
     element.style.top = (parentRect.height - rect.height) / 2 + parentRect.y + 'px';
+}
+
+function makeDraggable(element) {
+    makeTouchDraggable(element);
+    makeClickDraggable(element);
+}
+
+function makeTouchDraggable(element) {
+    let originTouchX;
+    let originTouchY;
+    element.addEventListener('touchstart', handleTouchStart);
+    element.addEventListener('touchmove', handleTouchMove);
 }
 
 function handleTouchStart(event) {
@@ -39,5 +47,37 @@ function handleTouchMove(event) {
         const rect = this.getBoundingClientRect();
         this.style.left = rect.x + 'px';
         this.style.top = rect.y + 'px';
+    }
+}
+
+function makeClickDraggable(element) {
+    element.isMoving = false;
+    element.addEventListener('mousedown', handleMouseDown);
+    document.addEventListener('mouseup', event=> handleMouseUp(event, element));
+    document.addEventListener('mousemove',event=> handleMouseMove(event, element));
+}
+
+function handleMouseDown(event) {
+    event.currentTarget.isMoving = true;
+}
+
+function handleMouseUp(event, element) {
+    element.isMoving = false;
+}
+
+function handleMouseMove(event, element) {
+    event.stopPropagation();
+    event.preventDefault();
+    if (element.isMoving) {
+        if (element.style.left && element.style.top) {
+            const offsetX = event.movementX;
+            const offsetY = event.movementY;
+            element.style.left = parseFloat(element.style.left.slice(0, -2)) + offsetX + 'px';
+            element.style.top = parseFloat(element.style.top.slice(0, -2)) + offsetY + 'px';
+        } else {
+            const rect = element.getBoundingClientRect();
+            element.style.left = rect.x + 'px';
+            element.style.top = rect.y + 'px';
+        }
     }
 }
